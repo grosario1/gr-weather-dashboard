28a8a43a9840832c55da97fbdc8d0599bad8c93b
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-07-29 12:30:09 -0400

    added some js to display weather & save searched city & display as a button

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,23 +21,21 @@ function getWeatherData(city) {
 
 var form = document.getElementById('user-form');
 var cityInput = document.getElementById('city-input');
-var weatherDisplay = document.getElementById('weather-display');
-var savedCitiesContainer = document.getElementById('saved-cities-container');
 
 function displayWeather(data) {
     var weatherInfoHTML = '';
     if (!data) {
-      weatherInfoHTML = '<p>City not found.</p>';
+        weatherInfoHTML = '<p>City not found.</p>';
     } else {
-      var cityName = data.name;
-      var date = new Date(data.dt * 1000).toLocaleDateString('en-US');
-      var iconCode = data.weather[0].icon;
-      var temperatureCelsius = data.main.temp;
-      var temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
-      var humidity = data.main.humidity;
-      var windSpeed = data.wind.speed;
-  
-      weatherInfoHTML = `
+        var cityName = data.name;
+        var date = new Date(data.dt * 1000).toLocaleDateString('en-US');
+        var iconCode = data.weather[0].icon;
+        var temperatureCelsius = data.main.temp;
+        var temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
+        var humidity = data.main.humidity;
+        var windSpeed = data.wind.speed;
+
+        weatherInfoHTML = `
         <div class="weather-info">
           <h2>${cityName} (${date})</h2>
           <div>
@@ -49,23 +47,57 @@ function displayWeather(data) {
         </div>
       `;
     }
-  
-    weatherDisplay.innerHTML = weatherInfoHTML;
-  }
-  
-  function celsiusToFahrenheit(temperatureCelsius) {
-    return (temperatureCelsius * 9/5) + 32;
-  }
 
-  function handleFormSubmit(event) {
+    weatherDisplay.innerHTML = weatherInfoHTML;
+}
+
+var weatherDisplay = document.getElementById('weather-display');
+
+function celsiusToFahrenheit(temperatureCelsius) {
+    return (temperatureCelsius * 9 / 5) + 32;
+}
+
+function handleFormSubmit(event) {
     event.preventDefault();
     var city = cityInput.value.trim();
     if (!city) {
-      return;
+        return;
     }
-  
-    getWeatherData(city);
-  }
 
-  form.addEventListener('submit', handleFormSubmit);
+    getWeatherData(city);
+}
+
+var savedCitiesContainer = document.getElementById('saved-cities-container');
+
+function saveCity(city) {
+    var savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
+    if (!savedCities.includes(city)) {
+        savedCities.push(city);
+        localStorage.setItem('savedCities', JSON.stringify(savedCities));
+    }
+}
+
+function updateSavedCitiesList() {
+    var savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
+    var savedCitiesHTML = '';
+    for (var i = 0; i < savedCities.length; i++) {
+        savedCitiesHTML += `
+        <button class="btn btn-secondary mr-2 mb-2 saved-city" data-city="${savedCities[i]}">${savedCities[i]}</button>
+      `;
+    }
+    savedCitiesContainer.innerHTML = savedCitiesHTML;
+}
+
+function handleSavedCityClick(event) {
+    if (event.target.classList.contains('saved-city')) {
+        var city = event.target.getAttribute('data-city');
+        getWeatherData(city);
+    }
+}
+
+savedCitiesContainer.addEventListener('click', handleSavedCityClick);
+
+updateSavedCitiesList();
+
+form.addEventListener('submit', handleFormSubmit);
 
