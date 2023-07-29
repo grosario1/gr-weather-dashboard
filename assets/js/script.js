29bc35c3a0f1bd0f906b924187065d58b42efc0b
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-07-29 12:04:39 -0400

    update html & added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,49 @@ var form = document.getElementById('user-form');
 var cityInput = document.getElementById('city-input');
 var weatherDisplay = document.getElementById('weather-display');
 var savedCitiesContainer = document.getElementById('saved-cities-container');
+
+function displayWeather(data) {
+    var weatherInfoHTML = '';
+    if (!data) {
+      weatherInfoHTML = '<p>City not found.</p>';
+    } else {
+      var cityName = data.name;
+      var date = new Date(data.dt * 1000).toLocaleDateString('en-US');
+      var iconCode = data.weather[0].icon;
+      var temperatureCelsius = data.main.temp;
+      var temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
+      var humidity = data.main.humidity;
+      var windSpeed = data.wind.speed;
+  
+      weatherInfoHTML = `
+        <div class="weather-info">
+          <h2>${cityName} (${date})</h2>
+          <div>
+            <img src="https://openweathermap.org/img/w/${iconCode}.png" alt="Weather Icon">
+            <p>Temperature: ${temperatureFahrenheit} °F</p>
+            <p>Humidity: ${humidity} %</p>
+            <p>Wind Speed: ${windSpeed} m/s</p>
+          </div>
+        </div>
+      `;
+    }
+  
+    weatherDisplay.innerHTML = weatherInfoHTML;
+  }
+  
+  function celsiusToFahrenheit(temperatureCelsius) {
+    return (temperatureCelsius * 9/5) + 32;
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    var city = cityInput.value.trim();
+    if (!city) {
+      return;
+    }
+  
+    getWeatherData(city);
+  }
+
+  form.addEventListener('submit', handleFormSubmit);
+
