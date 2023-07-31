f62b57f1feb803c17e0f30a40eadd86aa3652561
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-07-30 23:09:08 -0400

    added some additional comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 
 var apiKey = '30e0b19128baf005a61976d9fa1faeeb';
 
+// Function to get weather data for the city entered using the listed OpenWeather api. 
+
 function getWeatherData(city) {
     var request = new XMLHttpRequest();
+    // THis is a callback function 
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
+            // Checks is the api returns a response successfully. If succesfull, capture the data into a variable and parse the JSON output to get the city's coordinates.
+            // Saves the city input
             if (request.status === 200) {
                 var data = JSON.parse(request.responseText);
                 displayWeather(data);
@@ -12,7 +17,7 @@ function getWeatherData(city) {
                 saveCity(city);
                 updateSavedCitiesList();
             } else {
-                displayWeather(null);
+                displayWeather(null); // Clear weather display in case of error
                 displayForecast(null); // Clear forecast display in case of error
             }
         }
@@ -21,14 +26,17 @@ function getWeatherData(city) {
     request.send();
 }
 
+
 var form = document.getElementById('user-form');
 var cityInput = document.getElementById('city-input');
 
+// This function displays the weather info within the weather display card
 function displayWeather(data) {
     var weatherInfoHTML = '';
     if (!data) {
         weatherInfoHTML = '<p>City not found.</p>';
     } else {
+        // weather information from the API response & adds into a variable to use in places within the code
         var cityName = data.name;
         var date = new Date(data.dt * 1000).toLocaleDateString('en-US');
         var iconCode = data.weather[0].icon;
@@ -37,6 +45,7 @@ function displayWeather(data) {
         var humidity = data.main.humidity;
         var windSpeed = data.wind.speed;
 
+        // Populates the html elements needed to display the weather info within the page
         weatherInfoHTML = `
         <div class="weather-info">
           <h2>${cityName} (${date})</h2>
@@ -49,18 +58,20 @@ function displayWeather(data) {
         </div>
       `;
     }
-
+    // this displays the info on the page
     weatherDisplay.innerHTML = weatherInfoHTML;
 }
 
 var weatherDisplay = document.getElementById('weather-display');
 
+// Function to conver the temperature value from celsius to Fahrenheit
 function celsiusToFahrenheit(temperatureCelsius) {
     return (temperatureCelsius * 9 / 5) + 32;
 }
 
 var forecastDisplay = document.getElementById('forecast-display');
 
+// Function to get 5 day forecast of the city from the openWeathermap api
 function getForecastData(latitude, longitude) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -77,6 +88,7 @@ function getForecastData(latitude, longitude) {
     request.send();
 }
 
+// 
 function displayForecast(data) {
     var forecastInfoHTML = '';
     if (!data) {
@@ -94,7 +106,7 @@ function displayForecast(data) {
             var temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
             var humidity = forecastList[i].main.humidity;
             var windSpeed = forecastList[i].wind.speed;
-
+            // this generates the html element needed to display the 5 day forecast cards
             var forecastItemHTML = `
                 <div class="col-md-2 mb-4">
                     <div class="card forecast-item">
@@ -110,9 +122,11 @@ function displayForecast(data) {
         }
         forecastInfoHTML += '</div>';
     }
+    // this displays the info on the page
     forecastDisplay.innerHTML = forecastInfoHTML;
 }
-  
+
+// Funcrtion to handle the submit of the city entered
 function handleFormSubmit(event) {
     event.preventDefault();
     var city = cityInput.value.trim();
@@ -125,6 +139,7 @@ function handleFormSubmit(event) {
 
 var savedCitiesContainer = document.getElementById('saved-cities-container');
 
+// Function to save the search city into localstorage
 function saveCity(city) {
     var savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
     if (!savedCities.includes(city)) {
@@ -133,6 +148,7 @@ function saveCity(city) {
     }
 }
 
+// Function to update the list of the previous saved cities and display on the page
 function updateSavedCitiesList() {
     var savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
     var savedCitiesHTML = '';
@@ -144,6 +160,7 @@ function updateSavedCitiesList() {
     savedCitiesContainer.innerHTML = savedCitiesHTML;
 }
 
+// Function to handle clicks on the saved city buttons and display weather and forecast data
 function handleSavedCityClick(event) {
     if (event.target.classList.contains('saved-city')) {
         var city = event.target.getAttribute('data-city');
